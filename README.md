d3f76eac5102f0611ee599b0ce0ab32ffe489c76
Omar Anguas <fcomaranguas@gmail.com>
2021-02-20 20:35:59 -0600

    Changes

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 ## Small practice done with REST Countries API and fetch + async-await.
 
----
-
 ![alt](img.png)
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,48 +1,52 @@
 const d = document,
-    $content = d.querySelector('.countries'),
-    $fragment = d.createDocumentFragment(),
-    $template = d.getElementById('template').content;
+  $content = d.querySelector(".countries"),
+  $fragment = d.createDocumentFragment(),
+  $template = d.getElementById("template").content;
 
 const getCountry = async () => {
-    try {
-        const res = await fetch(`https://restcountries.eu/rest/v2/all`);
-        const countries = await res.json();
+  try {
+    const res = await fetch(`https://restcountries.eu/rest/v2/all`);
+    const countries = await res.json();
 
-        if (!res.ok) throw { status: res.status, statusText: res.statusText };
+    if (!res.ok) throw { status: res.status, statusText: res.statusText };
 
-        countries.forEach((country) => {
-            $template.querySelector('img').setAttribute('src', country.flag);
-            $template.querySelector('.country').textContent = country.name;
-            $template.querySelector('.capital').textContent = country.capital;
-            $template.querySelector('.region').textContent = country.region;
-            $template.querySelector('.subregion').textContent = country.subregion;
+    countries.forEach((country) => {
+      $template.querySelector("img").setAttribute("src", country.flag);
+      $template.querySelector(".country").textContent = country.name;
+      $template.querySelector(".capital").textContent = country.capital;
+      $template.querySelector(".region").textContent = country.region;
+      $template.querySelector(".subregion").textContent = country.subregion;
 
-            let $clone = d.importNode($template, true);
-            $fragment.appendChild($clone);
-        });
+      let $clone = d.importNode($template, true);
+      $fragment.appendChild($clone);
+    });
 
-        $content.appendChild($fragment);
-    } catch (err) {
-        let message = err.statusText || 'Ocurrió un error al solicitar datos';
-        $content.insertAdjacentHTML('afterend', `<p><b>Error ${err.status}: ${message}</b></p`);
-    }
-}
+    $content.appendChild($fragment);
+  } catch (err) {
+    let message = err.statusText || "Ocurrió un error al solicitar datos";
+    $content.insertAdjacentHTML(
+      "afterend",
+      `<p><b>Error ${err.status}: ${message}</b></p`
+    );
+  }
+};
 
-d.addEventListener('DOMContentLoaded', (e) => {
-    getCountry();
+d.addEventListener("DOMContentLoaded", (e) => {
+  getCountry();
 });
 
-const $cardFilter = d.querySelector('.card-filter');
+const $cardFilter = d.querySelector(".card-filter");
 
-d.addEventListener('keyup', (e) => {
-    if (e.target.matches('.card-filter')) {
-        const value = e.target.value;
-        const data = value.toLowerCase();
+d.addEventListener("keyup", (e) => {
+  if (e.target.matches(".card-filter")) {
+    const value = e.target.value;
+    const data = value.toLowerCase();
 
-        d.querySelectorAll('.card').forEach((card) => {
-            (card.textContent.toLowerCase().includes(e.target.value) || card.textContent.includes(e.target.value))
-                ? card.classList.remove('filter')
-                : card.classList.add('filter')
-        });
-    }
+    d.querySelectorAll(".card").forEach((card) => {
+      card.textContent.toLowerCase().includes(e.target.value) ||
+      card.textContent.includes(e.target.value)
+        ? card.classList.remove("filter")
+        : card.classList.add("filter");
+    });
+  }
 });
